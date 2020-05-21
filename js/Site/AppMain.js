e508243538417811bdf6ddd2ac5f42f4f023ad14
louie_kuo <louie_kuo@phalanity.com.tw>
2020-05-21 14:03:27 +0800

    v1

--- js/Site/AppMain.js
+++ js/Site/AppMain.js
@@ -10,15 +10,26 @@ AppMain.Initialize = function(){
 }
 
 AppMain.TextCustomerEmailAddress = $("#CustomerEmailAddress");
-AppMain.TextCustomerIdQRCode = $("#CustomerIdQRCode");
+AppMain.BlockCustomerIdQRCode = $("#CustomerIdQRCode");
 AppMain.BtnGetCustomerIdQRCode = $("#GetCustomerIdQRCode");
 AppMain.BindUIEvents = function(){
     AppMain.BtnGetCustomerIdQRCode.bind("click", AppMain.BtnGetCustomerIdQRCode_OnClicked);
+    AppMain.TextCustomerEmailAddress.bind("change", AppMain.TextCustomerEmailAddress_OnChange);
 }
 
 
 // ******************************
 AppMain.BtnGetCustomerIdQRCode_OnClicked = function(){
-    AppMain.TextCustomerIdQRCode.empty();
-    AppMain.TextCustomerIdQRCode.qrcode(btoa(AppMain.TextCustomerEmailAddress.val()));
+    AppMain.BlockCustomerIdQRCode.empty();
+    AppMain.BlockCustomerIdQRCode.qrcode(btoa(AppMain.TextCustomerEmailAddress.val()));
 }
+
+AppMain.TextCustomerEmailAddress_OnChange = function(){
+    
+}
+
+// ******************************
+AppMain.ValidateEmail = function(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
