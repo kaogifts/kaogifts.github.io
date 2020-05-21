797de97a767e386dfccfcd53c29f1fbd3d25836f
louie_kuo <louie_kuo@phalanity.com.tw>
2020-05-21 14:40:22 +0800

    add featrue for checking email

--- js/Site/AppMain.js
+++ js/Site/AppMain.js
@@ -12,9 +12,10 @@ AppMain.Initialize = function(){
 AppMain.TextCustomerEmailAddress = $("#CustomerEmailAddress");
 AppMain.BlockCustomerIdQRCode = $("#CustomerIdQRCode");
 AppMain.BtnGetCustomerIdQRCode = $("#GetCustomerIdQRCode");
+AppMain.ScrollTriggerGetCustomerIdQRCode = $("#GetCustomerIdQRCodeScrollTrigger");
 AppMain.BindUIEvents = function(){
     AppMain.BtnGetCustomerIdQRCode.bind("click", AppMain.BtnGetCustomerIdQRCode_OnClicked);
-    AppMain.TextCustomerEmailAddress.bind("change", AppMain.TextCustomerEmailAddress_OnChange);
+    AppMain.TextCustomerEmailAddress.bind("keyup", AppMain.TextCustomerEmailAddress_OnKeyPress);
 }
 
 
@@ -22,14 +23,20 @@ AppMain.BindUIEvents = function(){
 AppMain.BtnGetCustomerIdQRCode_OnClicked = function(){
     AppMain.BlockCustomerIdQRCode.empty();
     AppMain.BlockCustomerIdQRCode.qrcode(btoa(AppMain.TextCustomerEmailAddress.val()));
+    AppMain.ScrollTriggerGetCustomerIdQRCode.click();
 }
 
-AppMain.TextCustomerEmailAddress_OnChange = function(){
-    
+AppMain.TextCustomerEmailAddress_OnKeyPress = function(){
+    if(AppMain.ValidateEmail(AppMain.TextCustomerEmailAddress.val())){
+        AppMain.BtnGetCustomerIdQRCode.attr("disabled", false);
+    }
+    else{
+        AppMain.BtnGetCustomerIdQRCode.attr("disabled", true);
+    }
 }
 
 // ******************************
 AppMain.ValidateEmail = function(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
   }
